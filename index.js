492374636fc4dc4fa246740d077b4d08b68c2e48
Tianyou-Song <tim.d.song@gmail.com>
2022-10-08 01:09:39 -0700

    add defer

--- index.js
+++ index.js
@@ -28,57 +28,47 @@ client.once("ready", () => {
     console.log("Ready!");
 });
 
+
 client.on("interactionCreate", async (interaction) => {
-    try {
-        if (!interaction.isChatInputCommand()) {
-            return;
-        }
-
-        const { commandName, options } = interaction;
-
-        switch (commandName) {
-            case "r":
-                const params = {
-                    attempts: options.getNumber("attempts") || 1,
-                    complexity: options.getNumber("complexity") || 0,
-                    diceCount: options.getNumber("number_of_dice"),
-                    difficulty: options.getNumber("difficulty"),
-                    focus: options.getNumber("focus") || 0,
-                    has6as2: options.getBoolean("6_counts_as_2_successes") || false,
-                };
-
-                const worker = await spawn(new Worker('./worker.js'));
-                const resultString = await worker.roll(params);
-                Thread.terminate(worker);
-
-                let failed = 0;
-
-                while (failed < 3) {
-                    try {
-                        await interaction.reply(resultString);
-                        return;
-                    } catch (e) {
-                        failed++;
-                        console.error(e.message, `failed: ${failed}`);
-                    }
-                }
-
-                try {
-                    await interaction.error('Please try again.');
-                    return;
-                } catch (e) {
-                    console.error('can\'t send message')
-                }
-
-                return;
-            default:
-                return;
-        }
-    } catch (e) {
-        console.error(e.message, 'wtf');
+    if (!interaction.isChatInputCommand()) {
         return;
     }
 
+    const { commandName, options } = interaction;
+
+    switch (commandName) {
+        case "r":
+            try {
+                await interaction.deferReply();
+            } catch (e) {
+                console.error('interaction:', interaction.id, 'error in defer:', e.message);
+                return;
+            }
+
+            const params = {
+                attempts: options.getNumber("attempts") || 1,
+                complexity: options.getNumber("complexity") || 0,
+                diceCount: options.getNumber("number_of_dice"),
+                difficulty: options.getNumber("difficulty"),
+                focus: options.getNumber("focus") || 0,
+                has6as2: options.getBoolean("6_counts_as_2_successes") || false,
+            };
+
+            const worker = await spawn(new Worker('./worker.js'));
+            const resultString = await worker.roll(params);
+            Thread.terminate(worker);
+
+            try {
+                await interaction.editReply(resultString);
+                return;
+            } catch (e) {
+                console.error('interaction:', interaction.id, 'error in edit:', e.message);
+            }
+
+            return;
+        default:
+            return;
+    }
 });
 
 // Login to Discord with your client's token
